62e8307e1c62707f1b37466641f75d90b4d6b6e3
Nicholas-Pfo <nicholaspfohlmann@gmail.com>
2022-08-31 22:08:49 +0200

    Added faster kick, now checks if opponent is closer to ball

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/R2K/DefenseLongShotCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/R2K/DefenseLongShotCard.cpp
@@ -1,21 +1,26 @@
 /**
  * @file DefenseLongShotCard.cpp
  * @author Nicholas Pfohlmann, Adrian Müller 
- * @version: 1.0
+ * @version: 1.1
  *
  * Functions, values, side effects: 
  * - this card qualifies for the one DEFENSE player only who is closest to the ball
  * - it will take its time to make a long kick if no opppenent is too close 
+ * - it will make a faster, more unprecise version of the kick if opponent is closer
  *
  * Details: 
  * Purpose of this card is to clear the own field by a long shot.
- * - opponent is not to close (otherwise see ClearOwnHalf)
+ * - opponent is not to close (otherwise see ClearOwnHalf) and my distance to ball < opponents distance to ball
  * - isPlayBall()
  * - long shot to opponent goal, using correct foot: 
  *   - left or right is computed once, when the card is called for the first time
  *   - reason: while approaching the ball, the relative y-position might vary (for only a few centimeters),
  *             causing the skill to "freeze"
- * - defense will not  leaf own half, unless we are in OFFENSIVE or SPARSE Mode
+ * - defense will not leave own half, unless we are in OFFENSIVE or SPARSE Mode
+ * 
+ * 
+ * v1.1: added kick variations (precise and unprecise) and now using opponent-ball as distance
+ *       and check if I am closer to ball than opponent
  * 
  * After kick:
  * - a) card exists if role no longer is playsTheBall()
@@ -26,11 +31,9 @@
  * - However, if the ball is stuck, the flag may still be set, and the player will follow the ball
  * - This behavior is ok in OFFENSIVE and SPARSE mode
  * 
- * 
  * ToDo: 
  * check for free shoot vector and opt. change y-coordinate
  * check whether isDone () works correctly 
- * check for GoToBallAndKick options (eg "precisey") for better pormance 
  * 
  * provide max opp distance as LOAD_PARAMETERS
  */
@@ -48,7 +51,6 @@
 #include "Tools/Math/BHMath.h"
 
 // this is the R2K specific stuff
-
 #include "Representations/BehaviorControl/TeamBehaviorStatus.h" 
 #include "Representations/BehaviorControl/TeammateRoles.h"
 #include "Representations/BehaviorControl/PlayerRole.h"
@@ -70,6 +72,7 @@ CARD(DefenseLongShotCard,
     DEFINES_PARAMETERS(
     {,
       (float)(1000) minOppDistance,
+      (float)(2000) preciseKickDistance,
       (bool)(false) footIsSelected,  // freeze the first decision
       (bool)(true) leftFoot,
     }),
@@ -81,7 +84,7 @@ class DefenseLongShotCard : public DefenseLongShotCardBase
   {
     return
       thePlayerRole.playsTheBall() &&  // I am the striker
-      !opponentIsTooClose() &&  // see below: distace is minOppDistance
+      opponentIsTooClose() != 0 &&  // see below: min distance is minOppDistance
       theTeammateRoles.isTacticalDefense(theRobotInfo.number) && // my recent role
 
       //don't leaf own half, unless we are in OFFENSIVE or SPARSE Mode)
@@ -107,10 +110,14 @@ class DefenseLongShotCard : public DefenseLongShotCardBase
       footIsSelected = true;
       leftFoot = theFieldBall.positionRelative.y() < 0;
     }
-    if(leftFoot)
-      theGoToBallAndKickSkill(calcAngleToGoal(), KickInfo::forwardFastLeftLong);
-    else
-      theGoToBallAndKickSkill(calcAngleToGoal(), KickInfo::forwardFastRightLong);
+    KickInfo::KickType longShotKickType = leftFoot ? KickInfo::forwardFastLeftLong : KickInfo::forwardFastRightLong;
+    
+    switch (opponentIsTooClose())
+    {
+      case(1): theGoToBallAndKickSkill(calcAngleToGoal(), longShotKickType, false); break;
+      case(2): theGoToBallAndKickSkill(calcAngleToGoal(), longShotKickType, true); break;
+      default: theGoToBallAndKickSkill(calcAngleToGoal(), longShotKickType); break;
+    }
   }
 
   Angle calcAngleToGoal() const
@@ -118,12 +125,30 @@ class DefenseLongShotCard : public DefenseLongShotCardBase
     return (theRobotPose.inversePose * Vector2f(theFieldDimensions.xPosOpponentGroundLine, 0.f)).angle();
   }
 
-  bool opponentIsTooClose() const{
+  //returns an int value depending on how far the ball is away from the opponent
+  //0: opponent is too close or is closer at ball than me
+  //1: close  -> fast, unprecise kick
+  //2: far    -> precise kick
+  int opponentIsTooClose() const {
+    int lowestProximityIndex = 2;
+    float distanceToBall = theFieldBall.positionRelative.norm();
+
     for (const Obstacle& ob : theObstacleModel.obstacles)
-    {if (ob.isOpponent()) //tbd: check for teammate, add sector wheel)
-      return ob.center.norm() < minOppDistance;
+    {
+      if (ob.isOpponent()) { //tbd: check for teammate, add sector wheel)
+        float distanceOpponentToBall = (ob.center - theFieldBall.positionRelative).norm();
+
+        //is any opponent closer to ball than me or is too close to ball
+        if (distanceToBall >= distanceOpponentToBall || distanceOpponentToBall <= minOppDistance) {
+          return 0; 
+        }
+
+        if (distanceOpponentToBall <= preciseKickDistance) { //close enemy found
+          lowestProximityIndex = 1;
+        }
+      }
     }
-    return false;
+    return lowestProximityIndex;
   }
 };
 
--- Src/Modules/BehaviorControl/BehaviorControl/Cards/R2K/GoalieLongShotCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/R2K/GoalieLongShotCard.cpp
@@ -1,25 +1,27 @@
 /**
  * @file GoalieLongShotCard.cpp
  * @author Nicholas Pfohlmann, Adrian Müller
- * @version: 1.1
+ * @version: 1.2
  *
  * Functions, values, side effects:
- * - this card qualifies for the GOALIE player if the balls x-Pos is below the 
- *   circumcenter x-Pos of triangle formed by default pos of GL, DL, DR
+ * - this card qualifies for the GOALIE player if the ball is below the X-Pos of penalty mark
  * - it will take its time to make a long kick if no opppenent is too close
- *
+ * - it will make a faster, more unprecise version of the kick if opponent is closer
+ * 
  * Details:
  * Purpose of this card is to clear the own field by a long shot.
- * - opponent is not to close (otherwise see ClearOwnHalf)
+ * - opponent is not to close (otherwise see ClearOwnHalf) and my distance to ball < opponents distance to ball
  * - isPlayBall()
  * - long shot to opponent goal, using correct foot:
  *   - left or right is computed once, when the card is called for the first time
  *   - reason: while approaching the ball, the relative y-position might vary (for only a few centimeters),
  *             causing the skill to "freeze"
- * - goalie will not go beyond circumcenter x-Pos
+ * - goalie will not go beyond penalty mark
  *
  * 
  * v1.1: changed limit for goalie to xPosOwnPenaltyMark
+ * v1.2: added kick variations (precise and unprecise) and now using opponent-ball as distance
+ *       and check if I am closer to ball than opponent
  * 
  * After kick:
  * - a) card exists if role no longer is playsTheBall()
@@ -28,15 +30,11 @@
  * Note:
  * - because this is a long shot, the flag "playsTheBall" typicall is re-set after the shot, as a side effect.
  * - However, if the ball is stuck, the flag may still be set, and the player will follow the ball
- * - If goalie walks closer to ball it can occur that he is still playsTheBall but ball is beyond 
- *   the centroid x-Pos if ball was moved
  *
  * ToDo:
  * check for free shoot vector and opt. change y-coordinate
  * check whether isDone () works correctly
- * check for GoToBallAndKick options (eg "precisey") for better pormance
  * 
- * read default Positions from DefualtPoseProvider
  * provide max opp distance as LOAD_PARAMETERS
  *
  */
@@ -75,6 +73,7 @@ CARD(GoalieLongShotCard,
   DEFINES_PARAMETERS(
     {,
       (float)(1000) minOppDistance,
+      (float)(2000) preciseKickDistance,
       (bool)(false) footIsSelected,  // freeze the first decision
       (bool)(true) leftFoot,
     }),
@@ -86,10 +85,9 @@ class GoalieLongShotCard : public GoalieLongShotCardBase
   {
     return
       thePlayerRole.playsTheBall() &&  // I am the striker
-      !opponentIsTooClose() &&  // see below: distace is minOppDistance
+      opponentIsTooClose() != 0 &&  // see below: distace is minOppDistance
       theTeammateRoles.isTacticalGoalKeeper(theRobotInfo.number) &&// my recent role
 
-      //don't go beyond the circumcenter of triangle formed by the default poses of GL, DL, DR
       theFieldBall.positionOnField.x() <= theFieldDimensions.xPosOwnPenaltyMark &&   //don't go to far way out
       theFieldBall.positionOnField.x() >= theFieldDimensions.xPosOwnGroundLine;  // don't leave the field
   }
@@ -108,10 +106,14 @@ class GoalieLongShotCard : public GoalieLongShotCardBase
       footIsSelected = true;
       leftFoot = theFieldBall.positionRelative.y() < 0;
     }
-    if (leftFoot)
-      theGoToBallAndKickSkill(calcAngleToGoal(), KickInfo::forwardFastLeftLong);
-    else
-      theGoToBallAndKickSkill(calcAngleToGoal(), KickInfo::forwardFastRightLong);
+    KickInfo::KickType longShotKickType = leftFoot ? KickInfo::forwardFastLeftLong : KickInfo::forwardFastRightLong;
+
+    switch (opponentIsTooClose())
+    {
+      case(1): theGoToBallAndKickSkill(calcAngleToGoal(), longShotKickType, false); break;
+      case(2): theGoToBallAndKickSkill(calcAngleToGoal(), longShotKickType, true); break;
+      default: theGoToBallAndKickSkill(calcAngleToGoal(), longShotKickType); break;
+    }
   }
 
   Angle calcAngleToGoal() const
@@ -119,13 +121,30 @@ class GoalieLongShotCard : public GoalieLongShotCardBase
     return (theRobotPose.inversePose * Vector2f(theFieldDimensions.xPosOpponentGroundLine, 0.f)).angle();
   }
 
-  bool opponentIsTooClose() const {
+  //returns an int value depending on how far the ball is away from the opponent
+  //0: opponent is too close or is closer at ball than me
+  //1: close  -> fast, unprecise kick
+  //2: far    -> precise kick
+  int opponentIsTooClose() const {
+    int lowestProximityIndex = 2;
+    float distanceToBall = theFieldBall.positionRelative.norm();
+
     for (const Obstacle& ob : theObstacleModel.obstacles)
     {
-      if (ob.isOpponent()) //tbd: check for teammate, add sector wheel)
-        return ob.center.norm() < minOppDistance;
+      if (ob.isOpponent()) { //tbd: check for teammate, add sector wheel)
+        float distanceOpponentToBall = (ob.center - theFieldBall.positionRelative).norm();
+
+        //is any opponent closer to ball than me or is too close to ball
+        if (distanceToBall >= distanceOpponentToBall || distanceOpponentToBall <= minOppDistance) {
+          return 0;
+        }
+
+        if (distanceOpponentToBall <= preciseKickDistance) { //close enemy found
+          lowestProximityIndex = 1;
+        }
+      }
     }
-    return false;
+    return lowestProximityIndex;
   }
 };
 
