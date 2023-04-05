befbbdb66f061c70f10f3056355c3fbfef26cc1c
Wilhelm Simus <wisi1001@stud.hs-kl.de>
2023-04-05 10:07:28 +0200

    Copy and adjust TIRecorder from gore-end

---
+++ Src/Representations/BehaviorControl/TI/TIRecorder.h
@@ -0,0 +1,34 @@
+/**
+ * @file TIRecorder.h
+ * @author Andy Hobelsberger, Wilhelm Simus
+ * @brief This Representation stores data for the Recording of TI Sequences, along with pointers to functions that control the recording process. 
+ * @version 1.0
+ * @date 2023-04-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#pragma once
+
+#include "Representations/BehaviorControl/TI/TIData.h"
+#include "Tools/Streams/AutoStreamable.h"
+#include "Tools/Function.h"
+#include "Tools/RingBuffer.h"
+
+STREAMABLE(TIRecorder, 
+{
+    void clear() {
+        data.clear();
+        worldData.clear();
+    }
+
+    FUNCTION(void()) start;
+    FUNCTION(void()) stop;
+    FUNCTION(void(playbackAction& action)) changeAction;
+    FUNCTION(void()) save;
+    ,
+
+    (WorldData) worldData,
+    (PlaybackData) data, 
+    (bool) (false) recording, 
+});
