7f90ae945f8b40cbb5bdaad1747963e05365b298
Wilhelm Simus <wisi1001@stud.hs-kl.de>
2023-04-18 11:14:39 +0200

    Copy TeachinCard from GORE into TIRecorder Card, adjust files accordingly

---
+++ Src/Modules/BehaviorControl/BehaviorControl/TI/TIRecorderProvider.h
@@ -0,0 +1,11 @@
+/**
+ * @file TIRecordingProvider.cpp
+ * @author Wilhelm Simus (R2K)
+ * @brief This file is responsible for loading teach-in related data such as worldmodels and playback files
+ *
+ * @version 1.0
+ * @date 2023-04-18
+ *
+ */
+
+#pragma once
