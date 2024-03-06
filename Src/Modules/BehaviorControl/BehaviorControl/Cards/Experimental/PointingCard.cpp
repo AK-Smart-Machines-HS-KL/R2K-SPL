b6b669bb291d6d281d63404db3699a3fe59e4fe1
J0nathan79 <jobr1005@stud.hs-kl.de>
2024-03-06 18:27:50 +0100

    PointingCard cooldown Time

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/Experimental/PointingCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/Experimental/PointingCard.cpp
@@ -1,13 +1,13 @@
 /**
  * @file PointingCard.cpp
- * @author Jonathan Brauch    
- * @version 0.3
- * @date 2024-01-10
- * 
- * 
+ * @author Jonathan Brauch
+ * @version 1.0
+ * @date 2024-03-04
+ *
+ *
  */
 
-// Skills - Must be included BEFORE Card Base
+ // Skills - Must be included BEFORE Card Base
 #include "Representations/BehaviorControl/Skills.h"
 
 // Card Base
@@ -16,6 +16,7 @@
 
 // Representations
 #include "Representations/BehaviorControl/FieldBall.h"
+#include "Representations/Infrastructure/FrameInfo.h"
 
 
 #include "Representations/MotionControl/ArmMotionRequest.h"
@@ -24,70 +25,69 @@
 
 
 CARD(PointingCard,
-     {
-        ,
-        CALLS(Activity),
-        CALLS(LookForward),
-        CALLS(Stand),
-        CALLS(PointAt),
-        CALLS(PointAtWithArm),
-        CALLS(TurnToPoint),
-        REQUIRES(FieldBall),
+  {
+     ,
+     CALLS(Activity),
+     CALLS(LookForward),
+     CALLS(Stand),
+     CALLS(PointAt),
+     CALLS(PointAtWithArm),
+     CALLS(TurnToPoint),
+     REQUIRES(FieldBall),
+     REQUIRES(FrameInfo),
 
 
-        DEFINES_PARAMETERS(
-             {,
-                //Define Params here
+     DEFINES_PARAMETERS(
+          {,
+       //Define Params here
+       (int)(10000) cooldown,
+       (unsigned)(0) startTime,
+    }),
 
-             }),
+  /*
+  //Optionally, Load Config params here. DEFINES and LOADS can not be used together
+  LOADS_PARAMETERS(
+       {,
+          //Load Params here
+       }),
 
-        /*
-        //Optionally, Load Config params here. DEFINES and LOADS can not be used together
-        LOADS_PARAMETERS(
-             {,
-                //Load Params here
-             }),
-             
-        */
+  */
 
-     });
+  });
 
 class PointingCard : public PointingCardBase
 {
-  private:
+private:
   bool isActive = true;
 
-  public:
-  //always active
+public:
+  // always active
   bool preconditions() const override
   {
-    return isActive;
+    int timeSinceLastStart = theFrameInfo.getTimeSince(startTime);
+
+    return isActive && (timeSinceLastStart > cooldown); // Überprüfung der Ausführungscooldown-Zeit
   }
 
   bool postconditions() const override
   {
-    return true;   // set to true, when used as default card, ie, lowest card on stack
+    return true; // set to true, when used as the default card, i.e., the lowest card on the stack
   }
 
   void execute() override
   {
-    
     theActivitySkill(BehaviorStatus::testingBehavior);
-    // std::string s = "testingBehavior";
 
-    // Override these skills with the skills you wish to test
     theLookForwardSkill(); // Head Motion Request
-    // theStandSkill(); // Standard Motion Request
+    
 
     OUTPUT_TEXT("Test pointing");
-    
 
     // Die relative Ballposition holen
     Vector2f ballPositionRelative = theFieldBall.recentBallPositionRelative();
 
     // Die relative Ballposition im theTurnToPointSkill verwenden
     theTurnToPointSkill(ballPositionRelative);
-
 
     // Die Z-Koordinate auf 0 setzen, um einen Vector3f zu erstellen
     Vector3f ballPositionRelative3D(ballPositionRelative.x(), ballPositionRelative.y(), 0.0f);
@@ -96,23 +96,26 @@ class PointingCard : public PointingCardBase
     thePointAtSkill(ballPositionRelative3D);
 
 
-    //Mit festen Koordinaten 
 
-    //const Vector2f localPoint = Vector2f(-900.f, -500.f);
-    //theTurnToPointSkill(localPoint);
 
-    //const Vector3f localPoint1 = Vector3f(0.f, 3000.f, 355.f);
-    //thePointAtSkill(localPoint1);
 
-    //thePointAtWithArmSkill(Vector3f(1000.f, 0.f, 0.f), Arms::right);
-    //thePointAtWithArmSkill(Vector3f(1000.f, 0.f, 1000.f), Arms::left);
+    //Mit festen Koordinaten
+    // const Vector2f localPoint = Vector2f(-900.f, -500.f);
+    // theTurnToPointSkill(localPoint);
 
-    isActive = true;
+    // const Vector3f localPoint1 = Vector3f(0.f, 3000.f, 355.f);
+    // thePointAtSkill(localPoint1);
 
+    // thePointAtWithArmSkill(Vector3f(1000.f, 0.f, 0.f), Arms::right);
+    // thePointAtWithArmSkill(Vector3f(1000.f, 0.f, 1000.f), Arms::left);
+
+    // Aktualisieren Sie den Zeitpunkt der letzten Ausführung
+
+
+
+    startTime = theFrameInfo.time;
   }
 };
 
+
 MAKE_CARD(PointingCard);
-
-
-
