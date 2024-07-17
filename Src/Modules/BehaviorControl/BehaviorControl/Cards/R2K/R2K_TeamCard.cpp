19bb12ee41b24641b8427e1a28aa6b4f0d120ee8
R-ZWEI-Kickers <rzwei.kickers@hs-kl.de>
2024-07-17 11:16:35 +0200

    Tordiffenzenz ab -1 OFFENSIVE, ab +2 DEFENSIVE,sons NORMAL_GAME, SPARSE ab unter 4 Feldspielern

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/R2K/R2K_TeamCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/R2K/R2K_TeamCard.cpp
@@ -223,21 +223,21 @@ private:
 
     // to do: who is active - loop supp. index, number active
     // what if substitute goalie?
-    int teamBehaviorStatus = TeamBehaviorStatus::R2K_DEFENSIVE_GAME; // patch due to update errors
+    int teamBehaviorStatus = TeamBehaviorStatus::R2K_NORMAL_GAME; // patch due to update errors
 
-    // n vs. 2  || 3 vs 3 or less 
-    if (opp_penalties >= 18 || (own_penalties > 18 && opp_penalties > 18)) {  //undeployed robots count as penalized; the array is 20 bots long
     // HOT FIX
     // if(true) {
-      
-      /*
+    // n vs. 2  || 3 vs 3 or less 
+    if (opp_penalties >= 18 || (own_penalties > 18 && opp_penalties > 18)) {  //undeployed robots count as penalized; the array is 20 bots long
+
+      {  
       theTeamActivitySkill(TeamBehaviorStatus::R2K_SPARSE_GAME);
       teamBehaviorStatus = TeamBehaviorStatus::R2K_SPARSE_GAME;
-      */
+      }
   
-
+/*
       theTeamActivitySkill(TeamBehaviorStatus::R2K_NORMAL_GAME);
-      teamBehaviorStatus = TeamBehaviorStatus::R2K_NORMAL_GAME;
+      teamBehaviorStatus = TeamBehaviorStatus::R2K_NORMAL_GAME;*/
    
     }
     else {
@@ -251,8 +251,8 @@ private:
         theTeamActivitySkill(TeamBehaviorStatus::R2K_DEFENSIVE_GAME);
         teamBehaviorStatus = TeamBehaviorStatus::R2K_DEFENSIVE_GAME;
       } else {
-        theTeamActivitySkill(TeamBehaviorStatus::R2K_DEFENSIVE_GAME);
-        teamBehaviorStatus = TeamBehaviorStatus::R2K_DEFENSIVE_GAME;
+        theTeamActivitySkill(TeamBehaviorStatus::R2K_NORMAL_GAME);
+        teamBehaviorStatus = TeamBehaviorStatus::R2K_NORMAL_GAME;
       }
     }
 
