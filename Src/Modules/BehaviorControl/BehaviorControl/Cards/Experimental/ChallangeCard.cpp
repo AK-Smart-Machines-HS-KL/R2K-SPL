03ce5598e7f6ef1d8593b0e798efc0f8bcc455dd
defu1001 <defu1001@stud.hs-kl.de>
2024-12-17 12:49:21 +0100

    First iteration of intercept Ball

--- Src/Modules/BehaviorControl/BehaviorControl/Cards/Experimental/ChallangeCard.cpp
+++ Src/Modules/BehaviorControl/BehaviorControl/Cards/Experimental/ChallangeCard.cpp
@@ -1,10 +1,10 @@
 /**
- * @file TestCard.cpp
- * @author Andy Hobelsberger
- * @brief This card's preconditions are always true.
- *        Edit it for testing
+ * @file ChallangeCard.cpp
+ * @author Dennis Fuhrmann
+ * @brief Card for R2k Ball Challange 2025
+ *        Currently still in its infancy
  * @version 0.1
- * @date 2021-05-23
+ * @date 2024-16-12
  *
  *
  */
@@ -15,13 +15,17 @@
 // Card Base
 #include "Tools/BehaviorControl/Framework/Card/Card.h"
 #include "Tools/BehaviorControl/Framework/Card/CabslCard.h"
-#include "Representations/Configuration/FieldDimensions.h"
+
 
 // Representations
+#include "Representations/Configuration/FieldDimensions.h"
 #include "Representations/BehaviorControl/FieldBall.h"
 #include "Representations/Communication/RobotInfo.h"
 
 //#include <filesystem>
+#include "Tools/Modeling/BallPhysics.h"
+#include "Tools/Math/Eigen.h"
+#include "Tools/BehaviorControl/Interception.h"
 
 // Modify this card but don't commit changes to keep it clean for other developers
 // Also don't forget to put this card at the top of your Card Stack!
@@ -31,6 +35,9 @@ CARD(ChallangeCard,
        CALLS(Activity),
        CALLS(LookForward),
        CALLS(Stand),
+       CALLS(WalkToPoint),
+       CALLS(InterceptBall),
+       CALLS(LookAtBall),
        REQUIRES(FieldBall),
     });
 
@@ -51,8 +58,27 @@ class ChallangeCard : public ChallangeCardBase
 
     void execute() override
     {
+      theActivitySkill(BehaviorStatus::testingBehavior);
+
+      Vector2f ballpos =  theFieldBall.recentBallPositionRelative();
+
+      Vector2f intersectionwithownYAxis = theFieldBall.intersectionPositionWithOwnYAxis;
+
+      if (intersectionwithownYAxis != Vector2f::Zero()) {
+
+        //theWalkToPointSkill(Pose2f(0_deg, ballpos), 0.7f, true, true, false, true);
+
+        theInterceptBallSkill((unsigned) bit(Interception::walk), false, false);
 
 
+      }else{
+
+        //theWalkToPointSkill(Pose2f(45_deg, ballpos), 0.7f, true, true, false, true);
+
+        theLookAtBallSkill();
+        theStandSkill();
+
+      }
 
     }
 };
